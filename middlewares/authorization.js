00200503d7e769613df92b25130bd3fd22171738
Quentin-Jadoul <18155@ecam.be>
2023-05-11 23:20:16 +0200

    auth

---
+++ middlewares/authorization.js
@@ -0,0 +1,42 @@
+const db = require('../models/index.js')
+
+const jwt = require('jsonwebtoken')
+const jwtKey = 'my_secret_key'
+const jwtExpirySeconds = 300
+
+// Login a user
+exports.login = function(req, res) {
+    db.user.findOne({
+        where: {
+            username: req.body.username,
+            password: req.body.password
+        }
+    })
+    .then(function (user) {
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        } else {
+            const token = jwt.sign({ user_id: user.user_id }, jwtKey, {
+                algorithm: 'HS256',
+                expiresIn: jwtExpirySeconds
+            })
+            return res.status(200).send({ token: token });
+        }
+    })
+}
+
+// Authenticate a user
+exports.isAuthorized = function(req, res) {
+    if (typeof req.headers.authorization !== "undefined") {
+        let token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, jwtKey, (err, data) => {
+            if (err) {
+                return res.status(403).send({ message: "Not authorized" });
+            }
+            req.user = payload;
+            return next();
+        });
+    } else {
+        return res.status(403).send({ message: "Not authorized" });
+    }
+}
--- routes.js
+++ routes.js
@@ -6,13 +6,14 @@ let activityController = require('./controllers/activityController.js');
 let placeController = require('./controllers/placeController.js');
 let climbController = require('./controllers/climbController.js');
 
+const { login, isAuthorized} = require('./middlewares/authorization.js');
+
 // user
 router.post('/user', userController.createUser);
 router.get('/users', userController.getUsers);
 router.get('/user/:id', userController.getUser);
 router.patch('/user/:id', userController.updateUser);
 router.delete('/user/:id', userController.deleteUser);
-router.post('/user/login', userController.loginUser);
 
 // boulders
 router.post('/boulder', boulderController.createBoulder);
@@ -24,29 +25,32 @@ router.get('/boulders/place/:id', boulderController.getBouldersByPlace);
 router.get('/boulders/place/:id/count', boulderController.getBouldersCountByPlace);
 
 // activities
-router.post('/activity', activityController.createActivity);
+router.post('/activity', isAuthorized, activityController.createActivity);
 router.get('/activities', activityController.getActivities);
 router.get('/activity/:id', activityController.getActivity);
-router.patch('/activity/:id', activityController.updateActivity);
-router.delete('/activity/:id', activityController.deleteActivity);
+router.patch('/activity/:id', isAuthorized, activityController.updateActivity);
+router.delete('/activity/:id', isAuthorized, activityController.deleteActivity);
 router.get('/activities/user/:id', activityController.getActivitiesByUser);
 router.get('/activities/count', activityController.getActivitiesCount);
 
 // climbs
-router.post('/climb', climbController.createClimb);
+router.post('/climb', isAuthorized, climbController.createClimb);
 router.get('/climbs', climbController.getClimbs);
 router.get('/climb/:id', climbController.getClimb);
-router.patch('/climb/:id', climbController.updateClimb);
-router.delete('/climb/:id', climbController.deleteClimb);
+router.patch('/climb/:id', isAuthorized, climbController.updateClimb);
+router.delete('/climb/:id', isAuthorized, climbController.deleteClimb);
 router.get('/climbs/activity/:id', climbController.getClimbsByActivity);
 router.get('/climbs/boulder/:id', climbController.getClimbsByBoulder);
 
 // places
 router.post('/place', placeController.createPlace);
-router.get('/places', isAuthorized, placeController.getPlaces);
+router.get('/places', placeController.getPlaces);
 router.get('/place/:id', placeController.getPlace);
 router.patch('/place/:id', placeController.updatePlace);
 router.delete('/place/:id', placeController.deletePlace);
 router.get('/places/count', placeController.getPlacesCount);
+
+// Auth
+router.post('/login', login);
 
 module.exports = router;
